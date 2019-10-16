c08ee8ddc7bf851fe8c4c660c90112efe4689e4d
Kuntcheramen <kckhafra@gmail.com>
2019-10-16 10:09:50 -0400

    update

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
-    API_ENDPOINT: 'http://localhost:8000/api',
-    // API_ENDPOINT: 'https://gentle-caverns-98467.herokuapp.com/api',
+    // API_ENDPOINT: 'http://localhost:8000/api',
+    API_ENDPOINT: 'https://gentle-caverns-98467.herokuapp.com/api',
     TOKEN_KEY: 'ecoacme-auth-token',
     JWT_SECRET: process.env.JWT_SECRET|| "kc-secret",
 }
